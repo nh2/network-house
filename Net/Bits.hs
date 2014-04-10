c9aa7d842cab0bd76885652abbb46486c0791757
Niklas Hambüchen <mail@nh2.me>
2014-04-10 01:25:38 +0100

    Fix `bitSize` deprecation for GHC 7.8

--- Net/Bits.hs
+++ Net/Bits.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Net.Bits (module Net.Bits, module Data.Bits, module Data.Word) where
 
 import Data.Bits
@@ -5,14 +6,22 @@ import Data.Word
 import qualified Numeric
 
 
+#if __GLASGOW_HASKELL__ < 708
+-- GHC 7.8 introduced a new `finiteBitSize` in Data.Bits, deprecating `bitSize`.
+-- We emulate it in older versions.
+finiteBitSize :: Bits a => a -> Int
+finiteBitSize = bitSize
+#endif
+
+
 -- Manipulating bit arrays
 -- Warning: this actually uses lazyness :-)
 
 -- 0: least significant (right most in math notation)
-array .!. index         = let x = fromIntegral (array `shiftR` (bitSize x * index))
+array .!. index         = let x = fromIntegral (array `shiftR` (finiteBitSize x * index))
                           in x
 
-b1 `nextTo` b2          = (fromIntegral b1 `shiftL` bitSize b2) .|. fromIntegral b2
+b1 `nextTo` b2          = (fromIntegral b1 `shiftL` finiteBitSize b2) .|. fromIntegral b2
 
 
 
@@ -21,10 +30,10 @@ b1 `nextTo` b2          = (fromIntegral b1 `shiftL` bitSize b2) .|. fromIntegral
 catBits bs              = cat bs 0
   where cat [] a        = a
         cat (b:bs) a    = cat bs ((a `shiftL` size) .|. fromIntegral b)
-        size            = bitSize (head bs)
+        size            = finiteBitSize (head bs)
 
 
 showHex x               = Numeric.showHex x []
-showBin x               = reverse [ if x `testBit` n then '1' else '0' | n <- [0..bitSize x - 1] ]
+showBin x               = reverse [ if x `testBit` n then '1' else '0' | n <- [0..finiteBitSize x - 1] ]
 
 showHex' n = reverse . take n . (++repeat '0') . reverse . showHex
